0acdb9fd11e14336beb7aa4eb1bc435c250d724f
rachidaIb <rachida.ibeghouchene@gmail.com>
2020-10-27 21:29:38 -0400

    add changes

--- src/test/java/StepDfs/Login.java
+++ src/test/java/StepDfs/Login.java
@@ -16,19 +16,21 @@ public class Login {
         System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
          driver= new ChromeDriver();
          driver.get("https://www.facebook.com/");
-        Assert.assertTrue(driver.getTitle().contains("fcebook") );
+         System.out.println(driver.getTitle());
+        Assert.assertTrue(driver.getTitle().contains("Log In") );
 
 
     }
 
     @And("user logs with username, password")
     public void userLogsWithUsernamePassword() {
-        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("rachida.ibeghouchene@gmail.com");
+        driver.findElement(By.name("email")).sendKeys("rachida.ibeghouchene@gmail.com");
          driver.findElement(By.xpath("//input[@type='password']")).sendKeys("rachi1234");
          driver.findElement(By.xpath("//button[@name='login']")).click();
     }
 
     @Then("user should see message error")
     public void userShouldSeeMessageError() {
+        Assert.assertTrue(driver.findElement(By.xpath("//div[text()='The email and password you’ve entered is incorrect.]")).isDisplayed());
     }
 }
--- src/test/ressources/features/login.feature
+++ src/test/ressources/features/login.feature
@@ -3,3 +3,4 @@ Feature: Testing Facebbok Login
     Given user is on the facebook Login page
     And user logs with username, password
     Then user should see message error
+  
